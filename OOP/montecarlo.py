361ffedafb57fee30ef844ac81ba761ab6262321
Willi Löther <willi.loether@gmail.com>
2023-04-02 16:56:29 +0200

    Backup

--- OOP/montecarlo.py
+++ OOP/montecarlo.py
@@ -1,5 +1,6 @@
 import game as gm
 import random
+import time
 from math import log,sqrt,e,inf
 
 class Node:
@@ -15,20 +16,21 @@ def ucb1(curr_node):
     result = curr_node.exploitation_v+2*(sqrt(log(curr_node.num_parentVisit+e+(10e-6))/(curr_node.num_visit+(10e-10))))
     return result
 
-
 def expand(node):
     moves = node.state.possible_moves()
     for move in moves:
         child_state = node.state.make_move(move)
-        child_node = Node(child_state)
+        child_node = Node(gm.Game(child_state, not node.state.currentPlayer))
         child_node.parent = node
         node.children.append(child_node)
 
 def select(node):
-    while not node.state.game_over():
+    while not node.state.is_game_over():
         if not node.children:
-            # if the node has no children, it means it has not been explored yet, so return this node
-            return node
+            # if the node has no children, it means it has not been explored yet, so expand it
+            expand(node)
+            # select a random child node
+            node = random.choice(node.children)
         else:
             # select the child node with the highest UCB1 value
             ucb1_values = [ucb1(child_node) for child_node in node.children]
@@ -36,13 +38,16 @@ def select(node):
             max_ucb1_index = ucb1_values.index(max_ucb1_value)
             node = node.children[max_ucb1_index]
     
-    return node  # if the current node is terminal, return it
+    return node  # return a random child node if the current node is terminal
 
 def playout(node):
     curr_node = node
     while not curr_node.state.is_game_over():
-        curr_node = expand(curr_node)
-        curr_node = select(curr_node)
+        # expand the current node if it has not been explored yet
+        if not curr_node.children:
+            expand(curr_node)
+        # select a random child node
+        curr_node = random.choice(curr_node.children)
     result = curr_node.state.get_result()
     return result
 
@@ -53,8 +58,9 @@ def backpropagation(node, result):
         curr_node.exploitation_v += result
         curr_node = curr_node.parent
 
-def monte_carlo_tree_search(root):
-    for i in range(1000):
+def monte_carlo_tree_search(root, time_limit):
+    start_time = time.time()
+    while time.time() - start_time < time_limit:
         node = select(root)
         result = playout(node)
         backpropagation(node, result)
